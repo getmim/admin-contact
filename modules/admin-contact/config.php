0153b57b27770b5f323d3e22a9b3c10e7a1629c7
Iqbal Fauzi <iqbalfawz@gmail.com>
2023-08-10 17:47:48 +0700

    Fix bug on empty seen

--- modules/admin-contact/config.php
+++ modules/admin-contact/config.php
@@ -2,7 +2,7 @@
 
 return [
     '__name' => 'admin-contact',
-    '__version' => '0.0.2',
+    '__version' => '0.0.3',
     '__git' => 'git@github.com:getmim/admin-contact.git',
     '__license' => 'MIT',
     '__author' => [
